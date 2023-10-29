b17b2e5b200e7be034ac1bf8d57bfe5af15798c4
Yusen Zheng <ysyszheng@gmail.com>
2023-10-29 22:11:19 +0800

    fix get_dist_from_margin

--- utils/utils.py
+++ utils/utils.py
@@ -7,6 +7,9 @@ from typing import List
 import itertools
 
 
+NO_VERBOSE = False
+
+
 def fix_seed(seed):
     random.seed(seed)
     os.environ['PYTHONHASHSEED'] = str(seed)
@@ -17,14 +20,15 @@ def fix_seed(seed):
 
 
 def log(*args):
-    COLOR_RED = "\033[91m"
-    COLOR_RESET = "\033[0m"
-    fn = sys._getframe().f_back.f_code.co_filename
-    ln = sys._getframe().f_back.f_lineno
-    highlight_msg = 'File \"%s\", line %d\n' % (fn, ln)
+    if not NO_VERBOSE:
+        COLOR_RED = "\033[91m"
+        COLOR_RESET = "\033[0m"
+        fn = sys._getframe().f_back.f_code.co_filename
+        ln = sys._getframe().f_back.f_lineno
+        highlight_msg = 'File \"%s\", line %d\n' % (fn, ln)
 
-    colored_msg = COLOR_RED + highlight_msg + COLOR_RESET
-    print(colored_msg, *args)
+        colored_msg = COLOR_RED + highlight_msg + COLOR_RESET
+        print(colored_msg, *args)
 
 
 def normize(x, mean, std):
@@ -37,30 +41,25 @@ def unormize(x, mean, std):
 
 def get_dist_from_margin(b, m, q):
     assert b >= 1 and m >= b and len(q) == m
-    event_set = itertools.combinations(range(m), b)
+    event_set = list(itertools.combinations(range(m), b))
     if b == 1:
-        log(m,b)
-        return event_set, q
+        p = q
 
-    if m == b:
-        log(m,b)
-        return event_set, [1]
+    elif m == b:
+        p = [1]
     
-    if b > 1 and (b + 1) <= m and m < 2 * b:
-        log(m,b)
+    elif b > 1 and (b + 1) <= m and m < 2 * b:
         b_prime = m - b
         q_tilde = [1 - q[i] for i in range(len(q))]
         event_set_tilde, p_tilde = get_dist_from_margin(b_prime, m, q_tilde)
-        log(m,b)
         p = [0] * len(list(event_set))
         for i, combo in enumerate(event_set):
             for j, combo_tilde in enumerate(event_set_tilde):
-                if not set(combo).isdisjoint(set(combo_tilde)):
+                if set(combo).isdisjoint(set(combo_tilde)):
+                    log(i,j)
                     p[i] = p_tilde[j]
-        return event_set, p
 
-    if b > 1 and m >= 2 * b:
-        log(m,b)
+    elif b > 1 and m >= 2 * b:
         sorted_indices = sorted(range(len(q)), key=lambda i: q[i], reverse=True)
         sorted_q = [q[i] for i in sorted_indices]
 
@@ -70,11 +69,10 @@ def get_dist_from_margin(b, m, q):
         for i in range(b-1, m-1):
             q_tilde[i] = (sorted_q[i])/(1-sorted_q[m-1])
         event_set_tilde, p_tilde = get_dist_from_margin(b, m-1, q_tilde)
-        log(m,b)
         
         p = [0] * len(list(event_set))
         for i, combo in enumerate(event_set):
-            if set(combo) == set(range(b-1)+[m-1]):
+            if set(combo) == set(list(range(b-1))+[m-1]):
                 p[i] = sorted_q[m-1]
             if (m-1) not in combo:
                 for j, combo_tilde in enumerate(event_set_tilde):
@@ -83,10 +81,14 @@ def get_dist_from_margin(b, m, q):
 
         for i, combo in enumerate(event_set):
             event_set[i] = tuple([sorted_indices[i] for i in combo])
-        return event_set, p
     else:
         raise NotImplementedError
-
+    
+    return event_set, p
 
 if __name__ == '__main__':
-    log('hello world')
+    event_set, p = get_dist_from_margin(3,12,[.1,.1,.1,.2,.3,.3,.2,.4,.1,.3,.6,.3])
+    print(list(event_set))
+    print(sum(p))
+    for e, prob in zip(event_set, p):
+        print(e, prob)
